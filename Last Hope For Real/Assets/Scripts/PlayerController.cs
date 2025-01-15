59806d5f9637df860564fe41a7fa46bf43d7028b
Angel Joel Garcia Mordan <103035057+OznarG@users.noreply.github.com>
2025-01-15 00:49:30 -0500

    Jump animation adjustment

--- Last Hope For Real/Assets/Scripts/PlayerController.cs
+++ Last Hope For Real/Assets/Scripts/PlayerController.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour, IDamage
 {
     [Header("References")]
-    private CharacterController controller;
+    public CharacterController controller;
     [SerializeField] private Transform cameraThird;
 
     [Header("Movement Settings")]
