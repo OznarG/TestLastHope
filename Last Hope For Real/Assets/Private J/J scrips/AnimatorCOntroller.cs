bd2298a9f05f692d5094b67dc341d55003960334
Angel Joel Garcia Mordan <103035057+OznarG@users.noreply.github.com>
2025-01-15 01:13:08 -0500

    Update AnimatorCOntroller.cs

--- Last Hope For Real/Assets/Private J/J scrips/AnimatorCOntroller.cs
+++ Last Hope For Real/Assets/Private J/J scrips/AnimatorCOntroller.cs
@@ -33,7 +33,7 @@ public class AnimatorCOntroller : MonoBehaviour
         
 
 
-        if (Input.GetKeyDown(KeyCode.Space) && !gameManager.instance.playerScript.controller.isGrounded)
+        if (Input.GetKeyDown(KeyCode.Space) && !gameManager.instance.playerScript.controller.isGrounded )
         {
 
             //  transform.Translate(Vector3.up * 5 * Time.deltaTime);
